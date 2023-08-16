97908cd64b513b0a175fac5e4938e5c7a74da6b5
shubhambhargav10 <shubhambhargav10@gmail.com>
2023-08-16 17:37:43 +0530

    dk

--- Backend/Routes/clientRouter.js
+++ Backend/Routes/clientRouter.js
@@ -1,32 +1,71 @@
-const {Router} = require('express');
-const express = require('express')
+// const {Router} = require('express');
+// const express = require('express')
+// const { ClientModel } = require('../models/ClientModels');
+
+// const clientRouter = Router();
+
+// clientRouter.use(express.json());
+
+// clientRouter.get('/', async (req, res)=>{
+//     try {
+//         const allClients = await ClientModel.find();
+//         res.status(200).json(allClients);
+//     }
+//     catch (err) {
+//         res.status(500).json('internal server error ',err)
+//     }
+// })
+
+// clientRouter.post('/add', async (req, res) => {
+//     const clientDetails =  req.body;
+//     try {
+//         const clientToSave = new ClientModel(clientDetails);
+//         await clientToSave.save();
+//         res.status(200).json('data added successfully')
+//     }
+//     catch (err) {
+//         res.status(500).json('internal server error', err)
+//     }
+// })
+
+
+// module.exports = {clientRouter}
+
+const { Router } = require('express');
+const express = require('express');
 const { ClientModel } = require('../models/ClientModels');
+const { authentication } = require('../middlewares/authentication'); // Import the authentication middleware
 
 const clientRouter = Router();
 
 clientRouter.use(express.json());
 
-clientRouter.get('/', async (req, res)=>{
-    try {
-        const allClients = await ClientModel.find();
-        res.status(200).json(allClients);
-    }
-    catch (err) {
-        res.status(500).json('internal server error ',err)
-    }
-})
-
-clientRouter.post('/add', async (req, res) => {
-    const clientDetails =  req.body;
-    try {
-        const clientToSave = new ClientModel(clientDetails);
-        await clientToSave.save();
-        res.status(200).json('data added successfully')
-    }
-    catch (err) {
-        res.status(500).json('internal server error', err)
-    }
-})
+clientRouter.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://crm-system-retax-2bue.vercel.app');
+ 
+  next();
+});
 
 
-module.exports = {clientRouter}
+clientRouter.get('/', async (req, res) => {
+  try {
+    const allClients = await ClientModel.find();
+    res.status(200).json(allClients);
+  } catch (err) {
+    res.status(500).json('internal server error ', err);
+  }
+});
+
+
+clientRouter.post('/add', authentication, async (req, res) => {
+  const clientDetails = req.body;
+  try {
+    const clientToSave = new ClientModel(clientDetails);
+    await clientToSave.save();
+    res.status(200).json('data added successfully');
+  } catch (err) {
+    res.status(500).json('internal server error', err);
+  }
+});
+
+module.exports = { clientRouter };
