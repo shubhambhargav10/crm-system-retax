88fc46b55e8203210f2326811efbf61ecb324ebf
shubhambhargav10 <shubhambhargav10@gmail.com>
2023-08-16 12:36:41 +0530

    uri added

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,14 @@
-const mongoose  = require('mongoose');
-require("dotenv").config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const dbURI = process.env.MONGODB_URL;
 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const connection  = mongoose.connect(process.env.MONGODB_URL);
+const connection = mongoose.connection;
 
-module.exports = {connection}
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+module.exports = connection;
